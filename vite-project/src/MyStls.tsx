f14b2f4522f886a5937696b575f61914c0e8af43
ekankr2 <ekankr2@naver.com>
2022-06-29 16:43:29 +0900

    feat: select function

--- vite-project/src/MyStls.tsx
+++ vite-project/src/MyStls.tsx
@@ -1,14 +1,16 @@
-import React, {FC, Suspense, useEffect, useRef, useState} from 'react';
+import React, {Suspense, useEffect, useRef, useState} from 'react';
 import {useFrame, useLoader} from "@react-three/fiber";
 import {STLLoader} from "three/examples/jsm/loaders/STLLoader";
-import {Center} from "@react-three/drei";
+import {Center, Edges, Select} from "@react-three/drei";
 import {useControls} from "leva";
+import {Object3D} from "three";
 
 
 const MyStls = () => {
     const stl = useLoader(STLLoader, ['bone.stl', 'heart.stl', 'LLL.stl'])
     const group = useRef<any>(null!)
-    const [current, setCurrent] = useState(0)
+    const [select, setSelect] = useState<Object3D[]>([])
+    const [currentRotation, setCurrentRotation] = useState(0)
     const [color, setColor] = useState<string[]>([])
     const {spin, boneColor, heartColor, LLLColor} = useControls({
         spin: false,
@@ -17,30 +19,39 @@ const MyStls = () => {
         LLLColor: {value: '#d66154', label: 'LLL color'},
     });
 
+    console.log('stl: ',stl[0].uuid)
+
+    console.log(select[0]?.userData)
+
     useEffect(() => {
         setColor([boneColor, heartColor, LLLColor])
     }, [boneColor, heartColor, LLLColor])
 
     useFrame(() => {
         if (spin) {
-            setCurrent((prev: number) => prev + 0.01)
+            setCurrentRotation((prev: number) => prev + 0.01)
         }
-        group.current.rotation.z = current
+        group.current.rotation.z = currentRotation
     })
 
     return (
         <Suspense fallback={null}>
             <Center>
-                <group rotation={[-Math.PI / 2, 0, 0]} dispose={null} ref={group}>
-                    {
-                        stl.map((stl, idx) =>
-                            <mesh key={idx} scale={1.2} castShadow receiveShadow>
-                                <primitive attach="geometry" object={stl}></primitive>
-                                <meshStandardMaterial color={color[idx]}/>
-                            </mesh>
-                        )
-                    }
-                </group>
+                    <group rotation={[-Math.PI / 2, 0, 0]} dispose={null} ref={group}>
+                        <Select onChange={setSelect}>
+                        {
+                            stl.map((stl, idx) =>
+                                <mesh userData={{key: idx}} key={idx} scale={1.2} castShadow receiveShadow>
+                                    <primitive attach="geometry" object={stl}></primitive>
+                                    <meshStandardMaterial color={color[idx]}/>
+                                    <Edges visible={select[0]?.userData.key === idx}>
+                                        <meshBasicMaterial transparent color="#333" depthTest={false}/>
+                                    </Edges>
+                                </mesh>
+                            )
+                        }
+                        </Select>
+                    </group>
             </Center>
         </Suspense>
     )
